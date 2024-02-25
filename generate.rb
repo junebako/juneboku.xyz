5565247944a58ddd5b9cf178b245eb7105176022
june29 <june29.jp@gmail.com>
2024-02-25 18:09:32 +0900

    Update contents

--- generate.rb
+++ generate.rb
@@ -5,4 +5,21 @@ Bundler.require
 title = "🐣 juneboku.xyz 🌲"
 body = "juneboku.xyz"
 
+feed_urls = %w[
+  https://junebako.github.io/sff/june29/nikki.xml
+  https://junebako.github.io/sff/june29/zakki.xml
+  https://listen.style/p/kimagurefm/rss
+  https://listen.style/p/juneboku-life/rss
+  https://june29.substack.com/feed?sectionId=70158
+]
+
+feeds = feed_urls.map { |feed_url|
+  [
+    feed_url,
+    Feedjira.parse(Faraday.get(feed_url).body)
+  ]
+}
+
+last_updated_at = feeds.map { |_, feed| feed.entries.sort_by(&:published).last.published }.max
+
 puts ERB.new(File.read("template.erb")).result(binding)

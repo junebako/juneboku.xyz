47a847b780f85a224302db37038c53669bc969b6
june29 <june29.jp@gmail.com>
2024-02-25 17:16:12 +0900

    Coding...

--- generate.rb
+++ generate.rb
@@ -1,1 +1,12 @@
-puts "juneboku.xyz"
+require "bundler/inline"
+require "erb"
+
+gemfile do
+  gem "faraday"
+  gem "feedjira"
+end
+
+title = "🐣 juneboku.xyz 🌲"
+body = "juneboku.xyz"
+
+puts ERB.new(File.read("template.erb")).result(binding)
